0691286709a66ff79029ce4f997206606ea6348a
Голубев Артем <email>
2026-03-16 23:03:11 +0300

    Еще одна попытка))

--- src/homework1/Homework1_4.java
+++ src/homework1/Homework1_4.java
@@ -2,11 +2,9 @@ package homework1;
 
 public class Homework1_4 {
     public static void main(String[] args) {
-        double n = 3.85;
-        double b = 0.15;
-        double c = n;
-        int n = b+c;
-        System.out.println(n);
+        double n, b = 0,15;
+        n = 3.85 + b;
+        System.out.println(n); // n = 4
 
 
     }
