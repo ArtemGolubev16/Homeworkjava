ac3a5a82b18a3e6cf83b5b8bc67779d76d1022ab
Голубев Артем <email>
2026-03-17 21:56:24 +0300

    Еще одна попытка))

--- src/homework1/Homework1_5.java
+++ src/homework1/Homework1_5.java
@@ -4,7 +4,8 @@ public class Homework1_5 {
     public static void main(String[] args) {
         int q = 74;
         int w = 5;
-        int v = q/w;
-        System.out.println(q%w);
+        int v = q / w;
+        int r = q % w;
+        System.out.println(v+"."+r);
     }
 }
